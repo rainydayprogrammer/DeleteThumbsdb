5c89dcea288be301e2d8046a7f17e4ace0fd9389
rainydayprogrammer <idic@kcn.ne.jp>
2019-03-26 21:22:43 +0900

    ロジック追加

--- DeleteThumbsdb/Program.cs
+++ DeleteThumbsdb/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DeleteThumbsdb
 {
     class Program
     {
+        static string stdir = Directory.GetCurrentDirectory();
+
         static void Main(string[] args)
         {
+            DeleteFiles(stdir);
+        }
+
+        static void DeleteFiles(string dir)
+        {
+            var files = Directory.EnumerateFiles(dir);
+            string[] dirs = null;
+
+            foreach (var s in files)
+            {
+                if (s.EndsWith("Thumbs.db"))
+                {
+                    Console.WriteLine(s);
+                    File.Delete(s);
+                }
+            }
+
+            dirs = Directory.GetDirectories(dir);
+            if (dirs != null)
+            {
+                foreach (string s in dirs)
+                {
+                    DeleteFiles(s);
+                }
+            }
         }
     }
 }
